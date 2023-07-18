a07a6cc63593d886e95bec772c1255535b8f1478
Kisusyenni Venessa <kisusyenni01@gmail.com>
2023-07-18 19:01:55 +0700

    feat(main): Implement view binding to MainActivity

--- app/src/main/java/com/kisusyenni/disasterapp/view/MainActivity.kt
+++ app/src/main/java/com/kisusyenni/disasterapp/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.kisusyenni.disasterapp
+package com.kisusyenni.disasterapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,28 +8,23 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.kisusyenni.disasterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var mMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(binding.map.id) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    // Set Google maps
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
