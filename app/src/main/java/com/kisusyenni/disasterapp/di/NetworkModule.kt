75cf61024f45456d16b231d894aeb814134393ff
Kisusyenni Venessa <kisusyenni01@gmail.com>
2023-08-13 15:19:41 +0700

    refactor(module): Change dependency injection module from koin to hilt

--- app/src/main/java/com/kisusyenni/disasterapp/di/NetworkModule.kt
+++ app/src/main/java/com/kisusyenni/disasterapp/di/NetworkModule.kt
@@ -2,30 +2,73 @@ package com.kisusyenni.disasterapp.di
 
 import com.kisusyenni.disasterapp.BuildConfig
 import com.kisusyenni.disasterapp.data.api.ApiService
+import com.kisusyenni.disasterapp.data.repository.DisasterAppRepository
+import com.kisusyenni.disasterapp.data.repository.DisasterAppRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import org.koin.dsl.module
+import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideBaseUrl(): String {
+        return BuildConfig.API_URL
+    }
 
-val networkModule = module {
-    single {
-        val interceptor = if (BuildConfig.DEBUG) {
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return if (BuildConfig.DEBUG) {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         } else {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
         }
-        OkHttpClient.Builder()
-            .addInterceptor(interceptor)
-            .build()
+    }
+    @Singleton
+    @Provides
+    fun provideConvertorFactory(): Converter.Factory {
+        return GsonConverterFactory.create()
     }
 
-    single {
-        Retrofit.Builder()
-            .baseUrl(BuildConfig.API_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(get())
-            .build()
-            .create(ApiService::class.java)
+    @Singleton
+    @Provides
+    fun provideOkhttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+        return okHttpClient.build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        baseUrl: String,
+        converterFactory: Converter.Factory,
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(converterFactory)
+            .client(okHttpClient)
+        return retrofit.build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRepository(apiService: ApiService): DisasterAppRepository {
+        return DisasterAppRepositoryImpl(apiService)
     }
 }
--- app/src/main/java/com/kisusyenni/disasterapp/di/ViewModelModule.kt
+++ app/src/main/java/com/kisusyenni/disasterapp/di/ViewModelModule.kt
@@ -6,32 +6,20 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import com.kisusyenni.disasterapp.data.SettingsPreference
 import com.kisusyenni.disasterapp.data.SettingsPreferenceImpl
-import com.kisusyenni.disasterapp.data.repository.DisasterAppRepository
-import com.kisusyenni.disasterapp.data.repository.DisasterAppRepositoryImpl
-import com.kisusyenni.disasterapp.viewmodel.SettingsViewModel
-import com.kisusyenni.disasterapp.viewmodel.MainViewModel
-import org.koin.android.ext.koin.androidContext
-import org.koin.dsl.module
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
-
-val mainViewModelModule= module {
-    single<DisasterAppRepository> {
-        DisasterAppRepositoryImpl(get())
-    }
-
-    single {
-        MainViewModel(get())
-    }
-}
-
-
-val settingsViewModelModule = module {
-    single<SettingsPreference> {
-        SettingsPreferenceImpl(androidContext().dataStore)
-    }
-
-    single {
-        SettingsViewModel(get())
+@Module
+@InstallIn(SingletonComponent::class)
+object ViewModelModule {
+    @Singleton
+    @Provides
+    fun providePreference(@ApplicationContext applicationContext: Context): SettingsPreference {
+        return SettingsPreferenceImpl(applicationContext.dataStore)
     }
 }
